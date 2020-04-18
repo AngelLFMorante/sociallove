a4b3cf5a0454ba3c3024bd522933c1f216712cb5
Angel <1_web9_13@iesjovellanos.org>
2020-04-18 20:06:43 +0200

    No puedo hacer login with facebook

--- fb-login/fb-init.php
+++ fb-login/fb-init.php
@@ -7,14 +7,14 @@ session_start();
 require 'fb-login/vendor/autoload.php';
 
 $fb = new Facebook\Facebook([
-    'app_id' => '626744431482156',
-    'app_secret' => 'de520bd3a43baff2c79e63f04ffe9856',
+    'app_id' => '565161470795223',
+    'app_secret' => '0aa9c178b4196097f8ea5fce5b51debe',
     'default_graph_version' => 'v2.7'
 ]);
 
 $helper = $fb->getRedirectLoginHelper();
 $login_url = $helper->getLoginUrl("http://localhost/sociallove");
-
+/* ME da un error que no se que es ni en internet ni nada... */
 try {
     $accesToken = $helper->getAccessToken();
     if (isset($accesToken)) {
